22d5f83a21fd7fece22aa73e6f78a1a4fa07e9e3
phil-sorenson <phillip.sorenson31@gmail.com>
2022-09-26 23:53:50 -0500

    Created local_settings.py + added MySQL DB info + Updated settings.py with try/exception block & decimal to integer code

--- heroes_villains_project/settings.py
+++ heroes_villains_project/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x86n$fb*i#kur)7ohi59r*)p2csvr64to2p8zvbzt3^^=74$fc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -69,16 +67,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'heroes_villains_project.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+# Added this "setting" to return decimals as integers (Default Django returns decimals as strings) 
+REST_FRAMEWORK = {
+    'COERCE_DECIMAL_TO_STRING': False  
 }
+
 
 
 # Password validation
@@ -121,3 +114,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from heroes_villains_project.local_settings import *
+except ImportError:
+    pass
